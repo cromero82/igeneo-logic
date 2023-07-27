a7643d86ad77a5e6e7eacd682ac75fd92065b3d2
Carlos Romero <soportecarlosromero@gmail.com>
2023-07-27 12:40:50 -0500

    add favorito value (true/false) to colores service

--- logistic/src/main/java/com/ingeneo/logistic/domain/dto/ColoresDTO.java
+++ logistic/src/main/java/com/ingeneo/logistic/domain/dto/ColoresDTO.java
@@ -3,10 +3,13 @@ package com.ingeneo.logistic.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+
 @Getter
 @Setter
 public class ColoresDTO {
     private Long id;
     private String nombre;
     private String rgb;
+    private boolean favorito = false ;
 }
